519dbfb40ec074469906bfc9bb63efe4a6abf9ed
Thomas D'Ascoli <thomas@dasco.li>
2020-03-22 16:02:44 +0100

    fix

--- src/php/includes/style.cntnd_navigation_input.php
+++ src/php/includes/style.cntnd_navigation_input.php
@@ -1,3 +1,3 @@
 <style>
-<?= file_get_contents($cfgClient[$client]["module"]["path"].'cntnd_list/css/cntnd_navigation.css') ?>
+<?= file_get_contents($cfgClient[$client]["module"]["path"].'cntnd_navigation/css/cntnd_navigation.css') ?>
 </style>
